70d0786e9e9042fa5854b1c22504fb7a3853899b
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-04-11 21:57:37 +0900

    stream化して1ページずつ結果を出力するようにした

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const handlebars = require('handlebars')
 const _ = require('lodash')
-const crawle = require('./lib/crawle');
-
-const template = handlebars.compile(`
-|記事|ページランク|
-|---|---|
-{{#each this}}
-|[{{title}}]({{url}})|{{count}}|
-{{/each}}
-`)
+const recentItemsStream = require('./lib/recent-items-stream')
+const fetchPageRankStream = require('./lib/fetch-page-rank-stream')
+const templateStream = require('./lib/template-stream')
+const hasPageRankStream = require('./lib/has-page-rank-stream')
 
 const TAG = process.argv[2]
 
+recentItemsStream
+  .pipe(fetchPageRankStream)
+  .pipe(hasPageRankStream)
+  .pipe(templateStream)
+  .pipe(process.stdout)
+
 ;
 (async () => {
-  let ret = []
   // ITEM APIに指定するページの範囲
   // 1リクエストあたり100件の制限があるので、ページ数を変えて繰り返し実行します。
-  for (page of _.range(1, 3)) {
-    console.log('page', page);
-    ret = ret.concat(await crawle(page, TAG))
+  for (page of _.range(1, 5)) {
+    recentItemsStream.write([page, TAG])
   }
-  console.log(template(ret.filter(r => r.count > 0)));
 })()
