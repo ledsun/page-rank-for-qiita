adbbfc23ad2461688cde3daa008f98b16de770e6
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-04-11 23:58:25 +0900

    refactor

--- index.js
+++ index.js
@@ -1,49 +1,19 @@
-const _ = require('lodash')
+const PageStream = require('./lib/page-stream')
 const recentItemsStream = require('./lib/recent-items-stream')
 const fetchPageRankStream = require('./lib/fetch-page-rank-stream')
 const templateStream = require('./lib/template-stream')
 const hasPageRankStream = require('./lib/has-page-rank-stream')
-const {
-  Readable,
-  Transform
-} = require('stream')
+const LimitStream = require('./lib/limit-stream')
 
 const TAG = process.argv[2]
+const pageStream = new PageStream(TAG)
+const limitStream = new LimitStream(recentItemsStream, fetchPageRankStream)
 
-const pages = _.range(1, 101)
-const pageStream = new Readable({
-  objectMode: true,
-  read(size) {
-    const page = pages.shift()
-    if (page) {
-      this.push([page, TAG])
-    } else {
-      this.push(null)
-    }
-  }
-})
-
-// 一定数見つけたら検索をやめます
-let count = 0
-const limitStream = new Transform({
-  readableObjectMode: true,
-  writableObjectMode: true,
-  transform(chunk, encoding, callback) {
-    count++;
-    if (count > 35) {
-      // https://stackoverflow.com/questions/28621175/node-js-how-to-stop-a-piped-stream-conditionally
-      recentItemsStream.unpipe(fetchPageRankStream)
-      fetchPageRankStream.end()
-    }
-    callback()
-  }
-})
-
-const hoge = pageStream
+const pageRankStream = pageStream
   .pipe(recentItemsStream)
   .pipe(fetchPageRankStream)
   .pipe(hasPageRankStream)
   .pipe(templateStream)
 
-hoge.pipe(limitStream)
-hoge.pipe(process.stdout)
+pageRankStream.pipe(limitStream)
+pageRankStream.pipe(process.stdout)
