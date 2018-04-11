502cab44fc3a0cec57344e8a3103c577c198753b
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-04-11 19:39:24 +0900

    refactor

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const rp = require('request-promise')
-const sleep = require('sleep-promise')
 const handlebars = require('handlebars')
 const _ = require('lodash')
-const JSONStore = require('json-store');
-const db = JSONStore('/tmp/page-rank.json');
-const fetchPageRank = require('./fetch-page-rank');
+const crawle = require('./lib/crawle');
 
 const template = handlebars.compile(`
 |記事|ページランク|
@@ -21,56 +17,9 @@ const TAG = process.argv[2]
   let ret = []
   // ITEM APIに指定するページの範囲
   // 1リクエストあたり100件の制限があるので、ページ数を変えて繰り返し実行します。
-  for (page of _.range(1, 10)) {
+  for (page of _.range(1, 3)) {
     console.log('page', page);
-    ret = ret.concat(await crawle(page))
+    ret = ret.concat(await crawle(page, TAG))
   }
   console.log(template(ret.filter(r => r.count > 0)));
 })()
-
-async function crawle(page) {
-  const urls = await recentItems(100, page, TAG)
-  const items = []
-  for (const {
-      title,
-      url,
-      user
-    } of urls) {
-
-    // 毎回スクレイピングしていると遅いので、キャッシュする
-    let count = db.get(url)
-    if (count === undefined) {
-      count = await fetchPageRank(url, user)
-    }
-    db.set(url, count)
-
-    items.push({
-      title,
-      url,
-      count
-    })
-  }
-
-  return items
-}
-
-async function recentItems(perPage = 10, page = 1, tag = null) {
-  const uri = `https://qiita.com/api/v2/items?page=${page}&per_page=${perPage}${ tag ? `&query=tag%3A${tag}` : '' }`
-  try {
-    const body = await rp({
-      uri,
-      headers: {
-        'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
-      }
-    })
-    return JSON.parse(body)
-      .map(r => ({
-        url: r.url,
-        user: r.user.id,
-        title: r.title
-      }))
-  } catch (e) {
-    console.log(uri);
-    console.error(e);
-  }
-}
