da80c483f7e3505c38de737f6dfb36d2df430e16
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-04-23 15:10:56 +0900

    JavaScriptを編集しやすくするため、scriptファイルを分ける

---
+++ public/index.js
@@ -0,0 +1,31 @@
+const socket = new WebSocket(`ws://${location.hostname}:${location.port}`)
+
+// Connection opened
+socket.addEventListener('open', (event) => {
+  const params = (new URL(document.location))
+    .searchParams
+  const tagName = params.get('tag')
+  socket.send(tagName)
+})
+
+// Listen for messages
+socket.addEventListener('message', (event) => {
+  console.log('Message from server ', event.data)
+  const data = JSON.parse(event.data)
+  if (data.item) {
+    document.querySelector('.list')
+      .innerHTML += `
+    <li>
+      <div class="col-12">
+        <a href="${data.item.url}" target="_blank">${data.item.title}</a>
+      </div>
+    </li>
+    `
+  }
+})
+
+// Connection closed
+socket.addEventListener('close', (event) => {
+  document.querySelector('.status')
+    .innerText = 'Bye Bye, see you!'
+})
